18089c256fccb41c09518271e65d44c5a2a90b9c
Exp <17885980+exp111@users.noreply.github.com>
2025-06-27 16:39:19 +0200

    test

--- TranslationHelper/convert.py
+++ TranslationHelper/convert.py
@@ -18,45 +18,44 @@ with open(filename, encoding="utf-8") as f:
 def replaceSpecialIcons(text: str):
     return text.replace("*", "[star]")
 
-def parseCard(content: list[str]):
-    print(content)
+def parseCard(text: list[str]):
     index = 0
     # get title
     ## remove any whitespace or unique symbols. then convert to title-case 
-    title = content[index].strip("+ \n").title()
+    title = text[index].strip("+ \n").title()
     index += 1
     # get traits, if exist
     traits = ""
     # check for regex (TRAIT. TRAIT.)
-    if re.match(traitRegex, content[index]):
+    if re.match(traitRegex, text[index]):
         print("Found traits")
         # convert all traits to title-case, then rejoin into trait format
-        allTraits = content[index].split(".")
-        traits = str.join(". ", [t.strip().title() for t in allTraits])
+        allTraits = text[index].split(".")
+        traits = str.join(". ", [t.strip().title() for t in allTraits]).strip()
         index += 1
     # check if there is a boost
     boost = ""
-    textEndIndex = len(content)
+    textEndIndex = len(text)
     #TODO: flavor text
     # check for a line that starts with boost start
     for i in range(index, textEndIndex):
-        if content[i].startswith("* Boost:"):
+        if text[i].startswith("* Boost:"):
             print("Found boost")
-            boost = str.join("", content[i:textEndIndex]).replace("* Boost:", "<hr/>\n* <b>Boost</b>:")
+            boost = str.join("", text[i:textEndIndex]).replace("* Boost:", "<hr/>\n* <b>Boost</b>:")
             textEndIndex = i
             break
 
     # rest is text
-    text = str.join("", content[index:textEndIndex]).strip()
+    cardText = str.join("", text[index:textEndIndex]).strip()
 
     print(f"Title: {title}")
     print(f"Traits: {traits}")
-    print(f"Text: {text.replace("\n", "\\n")}")
+    print(f"Text: {cardText.replace("\n", "\\n")}")
     print(f"Boost: {boost.replace("\n", "\\n")}")
     return {
         "title": title,
         "traits": traits,
-        "text": replaceSpecialIcons(text),
+        "text": replaceSpecialIcons(cardText),
         "boost": replaceSpecialIcons(boost)
     }
 
@@ -70,7 +69,28 @@ def writeCard(card, file: TextIOWrapper):
         obj["traits"] = card["traits"]
     file.write(json.dumps(obj, indent="\t", ensure_ascii=False))
 
-card = parseCard(content)
+texts = []
+lines = []
+print(content)
+for text in content:
+    if len(text.strip()) == 0:
+        print("found end")
+        print(lines)
+        if len(lines) > 0:
+            texts.append(lines)
+            lines = []
+        continue
+    lines.append(text)
+if len(lines) > 0:
+    texts.append(lines)
+print(texts)
+print(f"found {len(texts)} parts")
 
-with open("out.txt", "w", encoding="utf-8") as f:
-    writeCard(card, f)
+cards = []
+for text in texts:
+    cards.append(parseCard(text))
+
+with open("out_" + filename, "w", encoding="utf-8") as f:
+    for card in cards:
+        writeCard(card, f)
+        f.write(",\n")
